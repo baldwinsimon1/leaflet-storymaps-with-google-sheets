d9b7fd4fb86cd3450245428894527693f78a1774
baldwinsimon1 <59477411+baldwinsimon1@users.noreply.github.com>
2020-01-03 14:04:01 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSB3yHBmNC2-UVEv_3MKCK9RF8d4lsBFHUHs8c7Ph1Jj-WFCDU1PoCrlulwU9CZOfb5vyTcbkyw6MXU/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Q57tOmJdwsq3VbU1WZwiOyGEmpVUpFKfGcuaD8M9dC0/edit#gid=0';
